f7e1de0ac8d2358e2f92a08b4c143a8155827005
Your Name <you@example.com>
2025-12-22 08:45:29 +0700

    add & update Exam

---
+++ Week 1 and 2/MaxA.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int max(int A[], int n) {
+	if (n == 0)
+		return A[0];
+	int y = max(A, n-1);
+	return y > A[n]? y:A[n];
+}
+
+int main() {
+	int n;
+	cout << "Input: ";
+	cin >> n;
+	int A[n];
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+	int result = max(A, n-1);
+	cout << "Output: " << result;
+	return 0;
+}
+
+
+
+
+
+
+
